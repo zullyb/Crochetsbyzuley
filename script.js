80a9ea4508d5f1c8376f5c3fc2014b22750cca1e
Zuleyma Baltazar <104181612+zullyb@users.noreply.github.com>
2026-04-16 18:35:36 -0500

    Update script.js
    changed code

--- script.js
+++ script.js
@@ -1,23 +1,13 @@
-function showSection(sectionId) {
-  const sections = document.querySelectorAll(".section");
+const menuToggle = document.getElementById("menu-toggle");
+const menuList = document.getElementById("menu-list");
+const menuLinks = document.querySelectorAll(".menu-list a");
 
-  sections.forEach(section => {
-    section.style.display = "none";
+menuToggle.addEventListener("click", () => {
+  menuList.classList.toggle("show");
+});
+
+menuLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    menuList.classList.remove("show");
   });
-
-  if (sectionId === "collection") {
-    document.getElementById("collection").style.display = "block";
-    document.getElementById("collection-gallery-1").style.display = "block";
-    document.getElementById("collection-gallery-2").style.display = "block";
-    document.getElementById("collection-gallery-3").style.display = "block";
-  } else if (sectionId === "socials") {
-    document.getElementById("socials-banner").style.display = "block";
-    document.getElementById("socials").style.display = "block";
-  } else {
-    document.getElementById(sectionId).style.display = "block";
-  }
-}
-
-document.addEventListener("DOMContentLoaded", () => {
-  showSection("intro");
 });
