62472c66207dc2d969b9845273ae2bfbefa3e7bc
Zuleyma Baltazar <104181612+zullyb@users.noreply.github.com>
2026-04-16 18:08:12 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 function showSection(sectionId) {
-  const sections = document.querySelectorAll('.section');
+  const allSections = document.querySelectorAll(".section");
 
-  sections.forEach(section => {
-    section.style.display = 'none';
+  allSections.forEach((section) => {
+    section.style.display = "none";
   });
 
-  document.getElementById(sectionId).style.display = 'block';
+  if (sectionId === "collection") {
+    document.getElementById("collection").style.display = "block";
+    document.getElementById("collection-gallery-1").style.display = "block";
+    document.getElementById("collection-gallery-2").style.display = "block";
+    document.getElementById("collection-gallery-3").style.display = "block";
+  } else if (sectionId === "socials") {
+    document.getElementById("socials-banner").style.display = "block";
+    document.getElementById("socials").style.display = "block";
+  } else {
+    document.getElementById(sectionId).style.display = "block";
+  }
 }
