e00f858649b6cbd8f7ddb85cd51e1bb56ad5b564
Zuleyma Baltazar <104181612+zullyb@users.noreply.github.com>
2026-04-16 18:40:05 -0500

    Update script.js
    added more

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
-const menuToggle = document.getElementById("menu-toggle");
-const menuList = document.getElementById("menu-list");
-const menuLinks = document.querySelectorAll(".menu-list a");
+document.addEventListener("DOMContentLoaded", () => {
+  const menuToggle = document.getElementById("menu-toggle");
+  const menuList = document.getElementById("menu-list");
+  const menuLinks = document.querySelectorAll(".menu-list a");
 
-menuToggle.addEventListener("click", () => {
-  menuList.classList.toggle("show");
-});
+  if (menuToggle) {
+    menuToggle.addEventListener("click", () => {
+      menuList.classList.toggle("show");
+    });
+  }
 
-menuLinks.forEach((link) => {
-  link.addEventListener("click", () => {
-    menuList.classList.remove("show");
+  menuLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+      menuList.classList.remove("show");
+    });
   });
 });
